2fb3a90275aa41680b9bce986ea73fbe147befe6
d33pster <d33pster@icloud.com>
2024-05-14 02:48:52 +0530

    add version and help

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,29 @@ fn main() {
     args.add("--scan", ArgumentDescription::new().short("-s"));
     args.add("--recursive", ArgumentDescription::new().short("-r"));
     args.add("--format", ArgumentDescription::new().short("-f"));
+    args.add("--help", ArgumentDescription::new().short("-h"));
+    args.add("--version", ArgumentDescription::new().short("-v"));
+
     
     args.analyse();
+
+    if args.ifarg_force("--version") {
+        println!("detecteff v0.1.0");
+        std::process::exit(0);
+    }
+
+    if args.ifarg_force("--help") {
+        println!("detecteff help");
+        println!("   -\n   [INFO]");
+        println!("   | -h, --help : show help text and exit.");
+        println!("   | -v, --version : show version and exit.");
+        println!("   -\n   [FLAG]");
+        println!("   | -r, --recursive : recursive mode. Default -> OFF");
+        println!("   | -f, --format : show formatted output. Default -> OFF");
+        println!("   -\n   [INPUT]");
+        println!("   | -s, --scan <directory> : scan the directory for duplicate files.");
+        std::process::exit(0);
+    }
 
     let mut recursive = false;
     let mut formatted = false;
